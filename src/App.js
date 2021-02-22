3420180e0232b1ff63d4f5d644aa17d10fc01ce6
olned <paulo.garcia.fl@gmail.com>
2021-02-21 21:42:13 -0300

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import * as Square from "./Components/Square"
+import { Patterns } from "./Patterns"
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X")
+  const [result, setResult] = useState({ winner: "", state: "" })
+
+  useEffect(() => {
+    checkWin();
+    checkTie();
+  }, [board])
+
+  useEffect(() => {
+    if (result.state != "") {
+      switch (result.winner) {
+        case "X":
+          alert("Game finished! Winning Player: O");
+          break;
+        case "O":
+          alert("Game finished! Winning Player: X");
+          break;
+        case "":
+          alert("Game finished! It's a tie!");
+          break;
+      }
+    }
+  }, [result])
+
+  const checkWin = () => {
+    Patterns.forEach((current) => {
+      const firstPlayer = board[current[0]];
+      if (firstPlayer == "") return;
+      let foundWinningPattern = true
+      current.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false
+        }
+      })
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "won" })
+      }
+    })
+  }
+
+  const checkTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    })
+
+    if (filled) {
+      setResult({ winner: "", state: "tie" })
+    }
+  }
+
+  const chooseSquare = (square) => {
+    setBoard(board.map((val, idx) => {
+      if (idx == square && val == "") {
+        if (player == "X") {
+          setPlayer("O");
+        } else {
+          setPlayer("X")
+        }
+        return player
+      }
+      return val;
+    }))
+  }
+
   return (
+    <div className="menu">
+      
+    </div>,
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        <div className="row">
+          <Square.s0 val={board[0]} chooseSquare={() => { chooseSquare(0) }} />
+          <Square.s1 val={board[1]} chooseSquare={() => { chooseSquare(1) }} />
+          <Square.s2 val={board[2]} chooseSquare={() => { chooseSquare(2) }} />
+        </div>
+        <div className="row">
+          <Square.s3 val={board[3]} chooseSquare={() => { chooseSquare(3) }} />
+          <Square.s4 val={board[4]} chooseSquare={() => { chooseSquare(4) }} />
+          <Square.s5 val={board[5]} chooseSquare={() => { chooseSquare(5) }} />
+        </div>
+        <div className="row">
+          <Square.s6 val={board[6]} chooseSquare={() => { chooseSquare(6) }} />
+          <Square.s7 val={board[7]} chooseSquare={() => { chooseSquare(7) }} />
+          <Square.s8 val={board[8]} chooseSquare={() => { chooseSquare(8) }} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Square.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import "../App.css"
+
+function s0( {val, chooseSquare} ) {
+    return (
+        <div id="s0"className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s1( {val, chooseSquare} ) {
+    return (
+        <div id="s1" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s2( {val, chooseSquare} ) {
+    return (
+        <div id="s2" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s3( {val, chooseSquare} ) {
+    return (
+        <div id="s3" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s4( {val, chooseSquare} ) {
+    return (
+        <div id="s4" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s5( {val, chooseSquare} ) {
+    return (
+        <div id="s5" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s6( {val, chooseSquare} ) {
+    return (
+        <div id="s6" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s7( {val, chooseSquare} ) {
+    return (
+        <div id="s7" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+
+function s8( {val, chooseSquare} ) {
+    return (
+        <div id="s8" className="square" onClick={chooseSquare}>
+            {val}
+        </div>
+    )
+}
+export {
+    s0,
+    s1,
+    s2,
+    s3,
+    s4,
+    s5,
+    s6,
+    s7,
+    s8,
+}
